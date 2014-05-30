a2bdbffb9ac811077963db241ff631eb8cb042f4
Chris Gill <chrisfgill@gmail.com>
2014-05-30 19:08:36 -0400

    modified broadcast/send methods to include from-chan. new method: take-data! for chan-data object retrieval

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject kuroshio "0.1.0-SNAPSHOT"
   :description "stream-like thread communication"
-  :url "http://github.com/viperscape/"
+  :url "https://github.com/viperscape/kuroshio"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
--- src/kuroshio/chan.clj
+++ src/kuroshio/chan.clj
@@ -5,39 +5,46 @@
   (put! [this ch v])
   (from [this] [this w])
   (from! [this] [this w])
-  (take! [this]))
+  (take! [this])
+  (take-data! [this]))
 
-(defn filter-chan [c s]
-  (map #(:v %) 
-       (remove #(not (or (= c (:c %))
-                         (= :all (:c %)))) s)))
+(defn filter-chan [c s as-data?]
+  (let [data (remove #(not (or (= c (:to-chan %))
+                               (and (= :all (:to-chan %))
+                                    (not(= c (:from-chan %)))))) s)]
+    (if as-data?
+      data
+      (map #(:v %) data) )))
 
-(defrecord chan-data [c v])
+(defrecord chan-data [to-chan v from-chan])
 (defn chan-data? [v]
   (= chan-data (type v)))
 
-(deftype c* [#^kuroshio.core.s* s]
+(deftype c* [^kuroshio.core.s* s]
   C*
-  (put! [this ch v] (k/put! s (chan-data. ch v)))
+  (put! [this ch v] (k/put! s (chan-data. ch v this)))
   (from! [this] (from! this nil))
-  (from! [this w] (filter-chan this (k/from! s w)))
+  (from! [this w] (filter-chan this (k/from! s w) false))
   (from [this] (from this nil))
-  (from [this w] (filter-chan this (k/from s w)))
-  (take! [this] (first(from! this :force))))
+  (from [this w] (filter-chan this (k/from s w) false))
+  (take! [this] (first(from! this :force)))
+  (take-data! [this] (first(filter-chan this (k/from! s :force) true))))
 
 (defn chan? [c]
   (= c* (type c)))
 
-(defn send! [#^c* c v]
-  (put! c c v))
-(defn broadcast! [#^c* c v]
-  (put! c :all v))
+(defn send! 
+  ([^c* c v] (put! c c v))
+  ([^c* tc v #^c* fc] (put! fc tc v)))
+
+(defn broadcast! [^c* c v]
+    (put! c :all v))
 
 (defn new-stream
   "convenience fn"
   ([] (k/new-stream))
-  ([#^kuroshio.core.s* s] (k/new-stream s)))
+  ([^kuroshio.core.s* s] (k/new-stream s)))
 
 (defn new-chan
-  ([#^kuroshio.core.s* s] (c*. (k/new-stream s))))
+  ([^kuroshio.core.s* s] (c*. (k/new-stream s))))
 
