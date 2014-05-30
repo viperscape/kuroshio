b82e68832605d6f3134f9d986914f6d50098a6df
Chris Gill <chrisfgill@gmail.com>
2014-05-30 09:49:10 -0400

    moved pool example into src

--- examples/pool.clj
+++ examples/pool.clj
@@ -1,21 +1,18 @@
 (ns examples.pool
-  (:require [kuroshio.core :as k]))
+  (:require [kuroshio.core :as k])
+  (:require [kuroshio.pool :as p :refer :all]))
 
-(defn broadcast! [pool v]
-  (doseq [s @pool] (k/put! s v)))
-
-(defn add-stream! [pool]
-  (let [s (k/new-s*)]
-    (swap! pool conj s)
-    s))
-
-(let [pool (atom [])
-      s1 (add-stream! pool)
-      s2 (add-stream! pool)]
-
+(let [p (new-pool)
+      s1 (k/new-s*)
+      s2 (add! p) ;;generates a stream, returns it
+      p2 (new-pool)
+      s3 (add! p2)]
+  (add! p s1)
   (k/put! s2 s1)
-  (future(do
-           (Thread/sleep 500)
-           (broadcast! pool :hi)))
-
-  (take 2 (k/from! s2 :force))) ;; (#<s* kuroshio.core.s*@68f8093f> :hi)
+  (broadcast! p :hi)
+  (remove! p s2)
+  (broadcast! p :hi-again)
+  (prn(k/from s2)) ;; (#<s* kuroshio.core.s*@1c4e4efb> :hi)
+  (merge-pool p p2)
+  (prn (members p) (members p2)) ;; #{#<s* kuroshio.core.s*@2ade3569> #<s* kuroshio.core.s*@6284967b>} #{}
+  (prn(member? p s1))) ;; true
---
+++ src/kuroshio/pool.clj
@@ -0,0 +1,35 @@
+(ns kuroshio.pool
+  (:require [kuroshio.core :as k]))
+
+(defrecord pool [#^clojure.lang.Atom p])
+
+(defn pool? [p]
+  (= pool (type p)))
+
+(defn broadcast! [#^pool p v]
+  (doseq [s @(:p p)] (k/put! s v)))
+
+(defn add! 
+  ([#^pool p] (add! p (k/new-s*)))
+  ([#^pool p #^kuroshio.core.s* s] 
+     (swap! (:p p) conj s)
+     s))
+
+(defn remove! [#^pool p #^kuroshio.core.s* s]
+  (swap! (:p p) disj s))
+
+(defn new-pool []
+  (let [p (pool. (atom #{}))]
+    p))
+
+(defn merge-pool [p1 p2]
+  (doseq [n @(:p p2)]
+    (remove! p2 n)
+    (add! p1 n))
+  p1)
+
+(defn members [#^pool p]
+   @(:p p))
+
+(defn member? [#^pool p #^kuroshio.core.s* s]
+  (not(nil?(@(:p p) s))))
