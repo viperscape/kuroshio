4c33215b73480fe0d514415f206b10f06894a44c
Chris Gill <chrisfgill@gmail.com>
2014-06-05 16:12:36 -0400

    update pub/sub example

--- examples/pubsub.clj
+++ examples/pubsub.clj
@@ -5,21 +5,24 @@
   "returns subscriber function to be run, pass in topic keyword and stream"
   [t s]
   {:pre [(keyword? t)]}
-  (let [_s (new-stream s)] ;;build our own stream copy to work on
+  (let [_s (new-stream s)] ;; build our own stream copy to work on
     (fn [] 
-      (->> (from! _s)
-           (map #(t %))
-           (remove nil?)))))
+      (->> (from! _s) ;; grab all avail, move head of stream copy
+           (filter t) ;; filter out anything but what we want
+           (map t))))) ;; just get the topic of interest
+           
 
 (let [s (new-stream)
-      ui-sub (sub :ui s)]
+      ui-sub (sub :ui s)
+      inp-sub (sub :input s)]
 
   (put! s {:something :updates})
+  (put! s {:ui :fade})
   (put! s {:input :space-bar
            :ui :clear})
-  (put! s {:submit :click
+  (put! s {:input :click
            :ui :draw})
 
-  (prn (take 5 (ui-sub))) ;; (:clear :draw)
-  (prn (take 5 (ui-sub))) ;; ()
-  (from s)) ;;({:something :updates} {:ui :clear, :input :space-bar} {:submit :click, :ui :draw})
+  (prn (first (ui-sub))) ;; :fade
+  (future (prn (inp-sub))) ;; (:space-bar :click)
+  (ui-sub)) ;; (:clear :draw)
