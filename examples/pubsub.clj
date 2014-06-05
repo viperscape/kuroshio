5bdb21f23142036efd6d6571e815b2e4014ce759
Chris Gill <chrisfgill@gmail.com>
2014-06-05 07:27:08 -0400

    pub/sub example

---
+++ examples/pubsub.clj
@@ -0,0 +1,27 @@
+(ns examples.pubsub
+  (:require [kuroshio.core :as k :refer :all]))
+
+(defn sub 
+  "returns subscriber function to be run, pass in topic keyword and stream"
+  [t s]
+  {:pre [(keyword? t)]}
+  (let [_s (new-stream s)] ;;build our own stream copy to work on
+    (fn [] 
+      (->>
+       s
+       from!
+       (map #(t %))
+       (remove nil?)))))
+
+(let [s (new-stream)
+      ui-sub (sub :ui s)]
+
+  (put! s {:something :updates})
+  (put! s {:input :space-bar
+           :ui :clear})
+  (put! s {:submit :click
+           :ui :draw})
+
+  (prn (take 5 (ui-sub))) ;; (:clear :draw)
+  (prn (take 5 (ui-sub))) ;; ()
+  (from s)) ;;({:something :updates} {:ui :clear, :input :space-bar} {:submit :click, :ui :draw})
